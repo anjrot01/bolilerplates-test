f973d65971e783430853e172e0fab54b351d2b0b
anjrotDev <antonio@anjrot.cl>
2021-04-03 23:56:28 -0400

    host

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const router = require("./routes/routes");
 const hbs = require("express-handlebars").create({});
 const app = express();
 
+const host = process.env.HOST || "0.0.0.0";
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json({ extended: true }));
 
-app.use("/api/v1", router());
+app.use("/api/v1", host, router());
 
 module.exports = app;

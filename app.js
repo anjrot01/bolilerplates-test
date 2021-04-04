02eef812567a4edaf2d490e95b50ec98a054e02e
anjrotDev <antonio@anjrot.cl>
2021-04-04 00:10:33 -0400

    fixing deploy errors

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const router = require("./routes/routes");
 const hbs = require("express-handlebars").create({});
 const app = express();
 
-const host = process.env.HOST || "0.0.0.0";
-
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json({ extended: true }));
 
-app.use("/api/v1", host, router());
+app.use("/api/v1", router());
 
 module.exports = app;
--- index.js
+++ index.js
@@ -4,5 +4,6 @@ require("./config/db");
 const environment = process.env.NODE_ENV || "development";
 
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || "0.0.0.0";
 
-app.listen(port, () => console.log(`Servidor levantado en el puerto ${port} en el ambiente de ${environment}`));
+app.listen(port, host, () => console.log(`Servidor levantado en el puerto ${port} en el ambiente de ${environment}`));

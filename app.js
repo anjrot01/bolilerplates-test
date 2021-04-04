7b9c21e50dd47b9855a129115cfd8260f911a10b
anjrotDev <antonio@anjrot.cl>
2021-04-04 18:06:47 -0400

    managing cors

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = require("./routes/routes");
 const hbs = require("express-handlebars").create({});
 const app = express();
+const cors = require("cors");
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.use(cors());
 
 app.use(express.json({ extended: true }));
 
